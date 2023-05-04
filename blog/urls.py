ffdbf6a241b6e41cc7b02280b9b6372a52a39be3
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-05-04 16:43:33 +0300

    added:pagination, class-based view for post list

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
-        path('', post_list, name='post_list'),
+        # path('', post_list, name='post_list'),
+        path('', PostListView.as_view(), name='post_list'),
         path('<int:year>/<int:month>/<int:day>/<slug:post>/',
              post_detail,
              name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from django.db.models import SlugField
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    # Pagination with for 3 blog posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # Exception handling for cases when the returned page does not exist
+        # If the page_number is out of range we deliver
+        # the last page of the results.
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # If page number value is not an integer, the returned page_number
+        # is the first page by default
+        posts = paginator.page(1)
+
     return render(
             request,
             'blog/post/list.html',
